52f58d9a91d8da6625b652ff6d5d6ef1eb28dc3a
Joseph Spencer <104227087+Jarseph@users.noreply.github.com>
2022-12-22 16:56:15 -0700

    Create searchByEmail.js

---
+++ searchByEmail.js
@@ -0,0 +1,17 @@
+/**
+ * Return the index of the customer with given email, otherwise -1.
+ * @param {string} email The email address to search for
+ * @param {*} customers The array to search
+ */
+function searchByEmail(email, customers) {
+  for (let index = 0; index < customers.length; index++) {
+    if (customers[index].email.toLowerCase() === email.toLowerCase()) {
+      return index;
+    }
+  }
+  return -1;
+}
+
+
+module.exports = searchByEmail;
+
